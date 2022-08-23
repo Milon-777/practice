21ba94930e302c3c43164ab6cef0170f2aef1246
Strizhak Roman <milon777.official@gmail.com>
2022-08-23 08:35:02 +0300

    Homework#2

--- js-practice/script.js
+++ js-practice/script.js
@@ -1,20 +1,56 @@
 "use strict";
 
-const numberOfFilms = +prompt("How many movies have you watched?");
-console.log(numberOfFilms);
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
   private: false,
 };
 
-const lastMovie1 = prompt("One of the last movies you watched?"),
-  lastMovieRating1 = +prompt("How much would you rate it?"),
-  lastMovie2 = prompt("One of the last movies you watched?"),
-  lastMovieRating2 = +prompt("How much would you rate it?");
-personalMovieDB.movies[lastMovie1] = lastMovieRating1;
-personalMovieDB.movies[lastMovie2] = lastMovieRating2;
+let lastMovie, lastMovieRating, numberOfFilms;
+
+for (let i = 0; i < 2; i++) {
+  if (i === 0) {
+    numberOfFilms = +prompt("How many movies have you watched?");
+    console.log(numberOfFilms);
+
+    while (numberOfFilms <= 0 || isNaN(numberOfFilms)) {
+      alert("Invalid number!");
+      numberOfFilms = +prompt("How many movies have you watched?");
+    }
+    personalMovieDB.count = numberOfFilms;
+
+    if (personalMovieDB.count < 10) {
+      alert("you have watched very few movies.");
+    } else if (personalMovieDB.count <= 30) {
+      alert("You`re a classic spectator!");
+    } else {
+      alert("You're a cinephile!");
+    }
+  }
+
+  lastMovie = prompt("One of the last movies you watched?");
+  if (lastMovie === null) {
+    break;
+  }
+
+  while (lastMovie.length > 50 || lastMovie.length <= 0) {
+    alert("Invalid name!");
+    lastMovie = prompt("One of the last movies you watched?");
+  }
+
+  lastMovieRating = +prompt("How much would you rate it?");
+  if (lastMovieRating === null) {
+    break;
+  }
+
+  while (lastMovieRating <= 0 || isNaN(lastMovieRating)) {
+    alert("Invalid number!");
+    lastMovieRating = +prompt("How much would you rate it?");
+  }
+
+  personalMovieDB.movies[lastMovie] = lastMovieRating;
+}
+
 console.log(personalMovieDB);

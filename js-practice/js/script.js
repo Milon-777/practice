dceb8c966f70789c4f88e339cd5280e8d870a33a
Strizhak Roman <milon777.official@gmail.com>
2022-09-01 12:23:31 +0300

    Homework#7

--- js-practice/js/script.js
+++ js-practice/js/script.js
@@ -1,104 +1,38 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
-"use strict";
-
 document.addEventListener("DOMContentLoaded", () => {
-  const movieDB = {
-    movies: [
-      "Логан",
-      "Лига справедливости",
-      "Ла-ла лэнд",
-      "Одержимость",
-      "Скотт Пилигрим против...",
-    ],
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader");
+
+  const hideContent = () => {
+    tabsContent.forEach((elem) => {
+      elem.classList.add("hide");
+      elem.classList.remove("show", "fade");
+    });
+
+    tabs.forEach((elem) => {
+      elem.classList.remove("tabheader__item_active");
+    });
   };
 
-  const adv = document.querySelectorAll(".promo__adv img"),
-    poster = document.querySelector(".promo__bg"),
-    genre = poster.querySelector(".promo__genre"),
-    movieList = document.querySelector(".promo__interactive-list"),
-    addForm = document.querySelector("form.add"),
-    addInput = addForm.querySelector(".adding__input"),
-    checkbox = addForm.querySelector('[type="checkbox"]');
+  const showTabContent = (i = 0) => {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  };
 
-  addForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+  tabsParent.addEventListener("click", (event) => {
+    const target = event.target;
 
-    let newMovie = addInput.value;
-    const favorite = checkbox.checked;
-
-    if (newMovie) {
-      if (newMovie.length > 21) {
-        newMovie = `${newMovie.substring(0, 22)}...`;
-      }
-
-      if (favorite) {
-        console.log("Добавляем любимый фильм");
-      }
-
-      movieDB.movies.push(newMovie);
-      sortArr(movieDB.movies);
-
-      createMovieList(movieDB.movies, movieList);
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((elem, i) => {
+        if (target == elem) {
+          hideContent();
+          showTabContent(i);
+        }
+      });
     }
-
-    event.target.reset();
   });
 
-  const deleteAdv = (arr) => {
-    arr.forEach((item) => {
-      item.remove();
-    });
-  };
-
-  const makeChanges = () => {
-    genre.textContent = "драма";
-
-    poster.style.backgroundImage = 'url("img/bg.jpg")';
-  };
-
-  const sortArr = (arr) => {
-    arr.sort();
-  };
-
-  function createMovieList(movies, list) {
-    list.innerHTML = "";
-    sortArr(movies);
-
-    movies.forEach((movie, i) => {
-      list.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${movie}
-          <div class="delete"></div>
-        </li>
-        `;
-    });
-
-    document.querySelectorAll(".delete").forEach((btn, i) => {
-      btn.addEventListener("click", () => {
-        btn.parentElement.remove();
-        movieDB.movies.splice(i, 1);
-
-        createMovieList(movies, list);
-      });
-    });
-  }
-
-  deleteAdv(adv);
-  makeChanges();
-  createMovieList(movieDB.movies, movieList);
+  hideContent();
+  showTabContent();
 });

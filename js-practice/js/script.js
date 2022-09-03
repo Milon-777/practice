6ee0e3591e4c80e645ecdf197a95402329f7355d
Strizhak Roman <milon777.official@gmail.com>
2022-09-03 16:47:22 +0300

    Homework#9

--- js-practice/js/script.js
+++ js-practice/js/script.js
@@ -38,14 +38,23 @@ document.addEventListener("DOMContentLoaded", () => {
   showTabContent();
 
   //Timer
-  const deadline = "2022-09-23";
+  const deadline = "2021-09-23";
 
   const getTimeRemaining = (endtime) => {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      minutes = Math.floor((t / 1000 / 60) % 60),
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+      minutes = Math.floor((t / 1000 / 60) % 60);
       seconds = Math.floor((t / 1000) % 60);
+    }
 
     return {
       total: t,
@@ -89,4 +98,144 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   setClock(".timer", deadline);
+
+  //Modal
+  const modalOpenBtns = document.querySelectorAll("[data-modal]"),
+    modalCloseBtn = document.querySelector("[data-close]"),
+    modal = document.querySelector(".modal");
+
+  const openModal = () => {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    clearInterval(modalTimerId);
+  };
+
+  const closeModal = () => {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = "visible";
+  };
+
+  const showModalByScroll = () => {
+    if (
+      window.scrollY + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  };
+
+  const modalTimerId = setTimeout(openModal, 10000);
+
+  modalOpenBtns.forEach((button) => {
+    button.addEventListener("click", openModal);
+  });
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
+
+  window.addEventListener("scroll", showModalByScroll);
+
+  //MenuCards
+
+  class MenuCard {
+    constructor(
+      title,
+      description,
+      imageSrc,
+      imageAlt,
+      price,
+      parentSelector,
+      ...classes
+    ) {
+      this.title = title;
+      this.description = description;
+      this.imageSrc = imageSrc;
+      this.imageAlt = imageAlt;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.classes = classes;
+      this.transfer = 27;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add();
+      } else {
+        this.classes.forEach((className) => {
+          element.classList.add(className);
+        });
+      }
+
+      element.innerHTML = `
+          <img src=${this.imageSrc} alt=${this.imageAlt} />
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.description}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>`;
+
+      this.parent.append(element);
+    }
+  }
+
+  new MenuCard(
+    "Меню “Фитнес”",
+    `Меню "Фитнес" - это новый подход к приготовлению блюд: больше
+    свежих овощей и фруктов. Продукт активных и здоровых людей. Это
+    абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+    "img/tabs/vegy.jpg",
+    "fitness_food",
+    9,
+    ".menu .container",
+    "menu__item"
+  ).render();
+
+  new MenuCard(
+    "Меню “Премиум”",
+    `В меню “Премиум” мы используем не только красивый дизайн упаковки,
+    но и качественное исполнение блюд. Красная рыба, морепродукты,
+    фрукты - ресторанное меню без похода в ресторан!`,
+    "img/tabs/elite.jpg",
+    "premium_food",
+    12,
+    ".menu .container",
+    "menu__item"
+  ).render();
+
+  new MenuCard(
+    "Меню “Постное”",
+    `Меню "Постное" - это тщательный подбор ингредиентов: полное
+    отсутствие продуктов животного происхождения, молоко из миндаля,
+    овса, кокоса или гречки, правильное количество белков за счет тофу
+    и импортных вегетарианских стейков.`,
+    "img/tabs/post.jpg",
+    "lenten_food",
+    15,
+    ".menu .container",
+    "menu__item"
+  ).render();
 });

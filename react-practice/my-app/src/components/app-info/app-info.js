9097d79583b63b61d414b1ced7f15a9093bc0865
Strizhak Roman <milon777.official@gmail.com>
2022-10-08 10:13:42 +0300

    Added simple project structure

---
+++ react-practice/my-app/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Облік співробітників у компанії N</h1>
+      <h2>Загальна кількість співробітників: </h2>
+      <h2>Премію отримають: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;

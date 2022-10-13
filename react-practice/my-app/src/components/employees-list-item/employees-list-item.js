eb8ca21507a8e3e4c2abb59139cd68304f621236
Strizhak Roman <milon777.official@gmail.com>
2022-10-13 21:12:10 +0300

    Rewrote employees-list-item component as a class

--- react-practice/my-app/src/components/employees-list-item/employees-list-item.js
+++ react-practice/my-app/src/components/employees-list-item/employees-list-item.js
@@ -1,29 +1,54 @@
+import { Component } from "react";
+
 import "./employees-list-item.css";
 
-const EmployeesListItem = ({ name, salary, isPromotion }) => {
-  let employeeType = "list-group-item d-flex justify-content-between";
-  if (isPromotion) employeeType += " increase";
+class EmployeesListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPromotion: false,
+    };
+  }
 
-  return (
-    <li className={employeeType}>
-      <span className="list-group-item-label">{name}</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary + "$"}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm">
-          <i className="fas fa-cookie"></i>
-        </button>
+  onPromotion = () => {
+    this.setState(({ isPromotion }) => ({
+      isPromotion: !isPromotion,
+    }));
+  };
 
-        <button type="button" className="btn-trash btn-sm">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
-};
+  render() {
+    const { name, salary } = this.props;
+    const { isPromotion } = this.state;
+    let employeeType = "list-group-item d-flex justify-content-between";
+    if (isPromotion) {
+      employeeType += " increase";
+    }
+
+    return (
+      <li className={employeeType}>
+        <span className="list-group-item-label">{name}</span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + "$"}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm"
+            onClick={this.onPromotion}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
 
 export default EmployeesListItem;

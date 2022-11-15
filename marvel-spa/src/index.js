a4b45abbd24c333e7f2f9690b0aac47f6b9f2629
Strizhak Roman <milon777.official@gmail.com>
2022-11-15 20:41:27 +0200

    Added getting a random character

--- marvel-spa/src/index.js
+++ marvel-spa/src/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 
 import "./style/style.scss";
-
-const marvelService = new MarvelService();
-
-marvelService
-  .getAllCharacters()
-  .then((result) =>
-    result.data.results.forEach((element) => console.log(element.name))
-  );
 
 ReactDOM.render(
   <React.StrictMode>
--- marvel-spa/src/services/MarvelService.js
+++ marvel-spa/src/services/MarvelService.js
@@ -11,16 +11,30 @@ class MarvelService {
     return await result.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const result = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&apikey=${process.env.REACT_APP_API_KEY}`
     );
+    return result.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getResource(
+  getCharacter = async (id) => {
+    const result = await this.getResource(
       `${this._apiBase}characters/${id}?apikey=${process.env.REACT_APP_API_KEY}`
     );
+    return this._transformCharacter(result.data.results[0]);
+  };
+
+  _transformCharacter = (character) => {
+    return {
+      name: character.name,
+      description: character.description
+        ? `${character.description.slice(0, 210)}...`
+        : "There is no description for this character",
+      thumbnail: character.thumbnail.path + "." + character.thumbnail.extension,
+      homepage: character.urls[0].url,
+      wiki: character.urls[1].url,
+    };
   };
 }
 
